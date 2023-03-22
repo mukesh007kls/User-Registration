b0da920c56b985fc02f3bd175ff3fa8a5bbeaf66
mukesh007kls <mallikanti2000@gmail.com>
2023-03-22 19:58:04 +0530

    ADDED:Code for parameterized Test Cases for email validity

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,18 +1,18 @@
 public class UserRegistration {
-    private static final String FIRST_NAME_REGEX="^[A-Z]{1}[a-z]{2,}";
-    private static final String LAST_NAME_REGEX=FIRST_NAME_REGEX;
-    private static final String EMAIL_ID_REGEX="^\\w+[._+-]?\\w+@\\w{3,}+[.]+[a-z]{2,3}[.]?[a-z]{0,3}$";
-    private static final String PHONE_NUMBER_REGEX="^[0-9]{1,3}[ ][0-9]{10}$";
-    private static final String PASSWORD_REGEX_ONE="^\\D{8,}$";
-    private static final String PASSWORD_REGEX_TWO="^(?=.*[A-Z])\\D{7,}$";
-    private static final String PASSWORD_REGEX_THREE="^(?=.*[A-Z])(?=.*\\d)\\w{6,}$";
-    private static final String PASSWORD_REGEX_FOUR="^(?=.*[!@#$%])(?=.*[A-Z])(?=.*\\d)(?=.*[a-z]).{8,30}$";
+    private static final String FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}";
+    private static final String LAST_NAME_REGEX = FIRST_NAME_REGEX;
+    private static final String EMAIL_ID_REGEX = "^\\w+[._+-]?\\w+@\\w{3,}+[.]+[a-z]{2,3}[.]?[a-z]{0,3}$";
+    private static final String PHONE_NUMBER_REGEX = "^[0-9]{1,3}[ ][0-9]{10}$";
+    private static final String PASSWORD_REGEX_ONE = "^\\D{8,}$";
+    private static final String PASSWORD_REGEX_TWO = "^(?=.*[A-Z])\\D{7,}$";
+    private static final String PASSWORD_REGEX_THREE = "^(?=.*[A-Z])(?=.*\\d)\\w{6,}$";
+    private static final String PASSWORD_REGEX_FOUR = "^(?=.*[!@#$%])(?=.*[A-Z])(?=.*\\d)(?=.*[a-z]).{8,30}$";
 
-    public boolean firstName(String firstName){
+    public boolean firstName(String firstName) {
         return firstName.matches(FIRST_NAME_REGEX);
     }
 
-    public boolean lastName(String lastName){
+    public boolean lastName(String lastName) {
         return lastName.matches(LAST_NAME_REGEX);
     }
 
@@ -20,23 +20,23 @@ public class UserRegistration {
         return emailID.matches(EMAIL_ID_REGEX);
     }
 
-    public boolean phoneNumber(String phoneNumber){
+    public boolean phoneNumber(String phoneNumber) {
         return phoneNumber.matches(PHONE_NUMBER_REGEX);
     }
 
-    public boolean passwordRule1(String password){
+    public boolean passwordRule1(String password) {
         return password.matches(PASSWORD_REGEX_ONE);
     }
 
-    public boolean passwordRule2(String password){
+    public boolean passwordRule2(String password) {
         return password.matches(PASSWORD_REGEX_TWO);
     }
 
-    public boolean passwordRule3(String password){
-        return  password.matches(PASSWORD_REGEX_THREE);
+    public boolean passwordRule3(String password) {
+        return password.matches(PASSWORD_REGEX_THREE);
     }
 
-    public boolean passwordRule4(String password){
-        return  password.matches(PASSWORD_REGEX_FOUR);
+    public boolean passwordRule4(String password) {
+        return password.matches(PASSWORD_REGEX_FOUR);
     }
 }
---
+++ src/test/java/EmailTest.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailTest {
+
+    private String emailID;
+    private boolean expectedResult;
+    private UserRegistration userRegistration;
+
+    public EmailTest(String emailID, boolean expectedResult) {
+        this.emailID = emailID;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void initialize(){
+        userRegistration=new UserRegistration();
+    }
+
+    @Parameterized.Parameters()
+    public static Collection testMailIDs(){
+        return Arrays.asList(new Object[][]{
+                {"mallikanti2000@gmail.com",true},
+                {"mallikanti-2000@gmail.com",true},
+                {"mallikanti.2000@gmail.com",true},
+                {"mallikanti@gmail.com",true},
+                {"mallikanti2000@gmail.com.in",true},
+                {"mallikanti2000@gmail1.com.in",true},
+                {"mallikanti+2000@gmail.com",true},
+                {"mallikanti2000gmail.com",false},
+                {"mallikanti2000@gmailcom",false},
+                {"mallikanti2000@gmail",false},
+                {"mallikanti2000",false},
+                {"mallikanti2000@gmail.2com",false},
+                {"mallikanti2000@g1.com",false}
+        });
+    }
+    @Test
+    public void givenMultipleEmailId_ShouldReturnTrueOrFalse(){
+        Assert.assertEquals(expectedResult,userRegistration.email(emailID));
+    }
+
+}
